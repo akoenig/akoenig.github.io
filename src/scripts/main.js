2d235e3b72102f8032cf3ffa8c46eb4c1970db0f
André König <andre.koenig@gmail.com>
2014-05-05 22:45:13 +0200

    Fixed comments.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,4 @@
-/*
+/*!
  * akoenig.github.io
  *
  * Copyright(c) 2014 André König <andre.koenig@posteo.de>
@@ -6,7 +6,7 @@
  *
  */
 
-/**
+/*!
  * @author André König <andre.koenig@posteo.de>
  *
  */
